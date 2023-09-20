c36c3c61a135d68da45d7315c956bdf6a7c730fb
ahphand-tamu <ahphand@tamu.edu>
2023-09-20 14:03:45 -0500

    DB setup 1

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import config
+from flask_migrate import Migrate
 
-CCEEMS = Flask(__name__)
+CCEMS = Flask(__name__)
+CCEMS.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
+db = SQLAlchemy(CCEMS)
+migrate = Migrate(CCEMS, db)
 
-@CCEEMS.route('/')
+@CCEMS.route('/')
 def landingRoute():
    return render_template('index.html')
 
 if __name__ == '__main__':
-    CCEEMS.run()
+    CCEMS.run()
