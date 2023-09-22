62cf0a40ba62809ad43da70a7e68edf3dd2ae91c
Tanmai Harish <11872828+tanmai-h@users.noreply.github.com>
2023-09-22 15:02:01 +0000

    Revert "DB setup 1"
    This reverts commit c36c3c61a135d68da45d7315c956bdf6a7c730fb.

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import config
-from flask_migrate import Migrate
 
-CCEMS = Flask(__name__)
-CCEMS.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
-db = SQLAlchemy(CCEMS)
-migrate = Migrate(CCEMS, db)
+CCEEMS = Flask(__name__)
 
-@CCEMS.route('/')
+@CCEEMS.route('/')
 def landingRoute():
    return render_template('index.html')
 
 if __name__ == '__main__':
-    CCEMS.run()
+    CCEEMS.run()
