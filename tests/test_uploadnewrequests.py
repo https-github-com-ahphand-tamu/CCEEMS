c99c2088eefb5698bfb726014a3828ac3fe33a43
ahphand-tamu <ahphand@tamu.edu>
2023-09-27 16:45:03 -0500

    Atharva: test cases 1

---
+++ tests/test_uploadnewrequests.py
@@ -0,0 +1,31 @@
+import unittest
+from flask import Flask
+from main import app
+
+class TestUploadNewRequestsFlow(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+    
+    def test_upload_new_requests_page(self):
+        response = self.app.get('/upload-new-requests')
+        self.assertEqual(response.status_code, 200)
+
+    def test_upload_valid_csv_file(self):
+        #E:/TAMU/fall-23/csce-606-se/sample_new_cases.xlsx
+        with open(r'./testfiles/sample_new_cases.xlsx', 'rb') as file:
+            data = {'new-requests': file}
+            response = self.app.post('/upload-new-requests', data=data, content_type='multipart/form-data')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('text/html', response.content_type)
+
+    def test_upload_invalid_file_format(self):
+        with open(r'./testfiles/CrisisInSoftware.pdf', 'rb') as file:
+            data = {'new-requests': file}
+            response = self.app.post('/upload-new-requests', data=data, content_type='multipart/form-data')
+        
+        self.assertEqual(response.status_code, 501)
+   
+if __name__ == '__main__':
+    unittest.main()
