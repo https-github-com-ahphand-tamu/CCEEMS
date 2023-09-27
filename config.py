f5e1acca825d8f62d773b13ba30cc4f4226c0d4c
Tanmai Harish <11872828+tanmai-h@users.noreply.github.com>
2023-09-27 01:04:03 +0000

    uri

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
