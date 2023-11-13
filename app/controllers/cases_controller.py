657f3fd6aa0744f5a42d1ebe68332cfff6131205
Tanmai Harish <11872828+tanmai-h@users.noreply.github.com>
2023-11-13 20:18:54 +0000

    table edit assign hiding

--- app/controllers/cases_controller.py
+++ app/controllers/cases_controller.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, render_template, jsonify
 from flask_login import current_user
 from app.models import Case, User, PacketReturnStatus, Decision
 from app import db
-import logging
 from datetime import datetime
 
 assign = Blueprint('assign', __name__)
@@ -20,7 +19,6 @@ def edit_case():
     data = request.json
 
     # Extract and validate data
-    outreach_date_str = data.get('outreachDate')
     num_children_enrolled_str = data.get('numChildrenEnrolled')
     decision_date_str = data.get('decisionDate')
     packet_received_date_str = data.get('packetReceivedDate')
@@ -32,40 +30,38 @@ def edit_case():
 
     # Validate and convert strings to proper types
     try:
-        outreach_date = datetime.strptime(outreach_date_str, '%Y-%m-%d')
         decision_date = datetime.strptime(decision_date_str, '%Y-%m-%d' ) if decision_date_str else None
         packet_received_date = datetime.strptime(packet_received_date_str, '%Y-%m-%d') if packet_received_date_str else None
     except (ValueError, TypeError) as _:
         return jsonify({"status": "Error", "message": "decision/packet received date provided must valid dates"}), 400
+    
+    case_id = data.get('caseId')
+    case_to_edit = Case.query.filter_by(id=case_id).first()
+    
+    if not case_to_edit:
+        return jsonify({"status": "Error", "message": "Case not found"}), 404
 
+    outreach_date = case_to_edit.outreach_date
     # Validate that decision and packet_received_date are not before outreach_date
-    if decision_date and decision_date < outreach_date:
+    if decision_date and decision_date.date() < outreach_date:
         return jsonify({"status": "Error", "message": "Decision date cannot be before outreach date"}), 400
 
-    if packet_received_date and packet_received_date < outreach_date:
+    if packet_received_date and packet_received_date.date() < outreach_date:
         return jsonify({"status": "Error", "message": "Packet received date cannot be before outreach date"}), 400
 
     # Get the case ID from the data
-    case_id = data.get('caseId')
+    case_to_edit.packet_return_status = data.get('packetReturnStatus')
+    case_to_edit.packet_received_date = data.get('packetReceivedDate')
+    case_to_edit.decision = data.get('decision')
+    case_to_edit.num_children_enrolled = num_children_enrolled
+    case_to_edit.decision_date = decision_date
+    case_to_edit.not_enrolled_reason = data.get('notEnrolledReason')
 
-    # Find the case in the database
-    case_to_edit = Case.query.filter_by(id=case_id).first()
-
-    if case_to_edit:
-        # Update all the case fields
-        case_to_edit.packet_return_status = data.get('packetReturnStatus')
-        case_to_edit.packet_received_date = data.get('packetReceivedDate')
-        case_to_edit.decision = data.get('decision')
-        case_to_edit.num_children_enrolled = num_children_enrolled
-        case_to_edit.decision_date = decision_date
-        case_to_edit.not_enrolled_reason = data.get('notEnrolledReason')
-
-        # Save changes to the database
+    try:
         db.session.commit()
-
         return jsonify({"status": "OK"}), 200
-    else:
-        return jsonify({"status": "Error", "message": "Case not found"}), 404
+    except Exception as e:
+        return jsonify({"status": "Error", "message": e}), 400
 
 @assign.route('/assign_request/<int:request_id>', methods=['POST'])
 def assign_request(request_id):
