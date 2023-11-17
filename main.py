2d4f6e2b41afe12aec88ebb1f6e2088814d34229
Tanmai Harish <11872828+tanmai-h@users.noreply.github.com>
2023-11-17 22:55:29 +0000

    log

--- main.py
+++ main.py
@@ -28,8 +28,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
